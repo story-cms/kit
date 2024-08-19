8a5337a9bcd509545837e326880bb399e468076f
Jannie Theunissen <jannie@scoutredeem.co>
2024-08-19 16:38:05 +0200

    cleaned up module resolution

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// https://tailwindcss.com/docs/guides/vite#vue
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
