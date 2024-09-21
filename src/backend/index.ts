2d3bc51910b67d9984394af1c0c10af99462c8ca
Jannie Theunissen <jannie@scoutredeem.co>
2024-09-21 16:34:55 +0200

    feat: backend build

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,4 +1,5 @@
 // Adonis plugin:
+export * from '../interfaces.js';
 export { configure } from './configure.js';
 export { stubsRoot } from './stubs/main.js';
 export { defineConfig } from './define_config.js';
