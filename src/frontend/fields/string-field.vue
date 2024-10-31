84e220a31f2ee6373ef50645e0ce1a8b782b7fd3
Timothy Koech <timosville@gmail.com>
2024-10-31 16:00:51 +0300

    feat: port select field

--- src/frontend/fields/string-field.vue
+++ src/frontend/fields/string-field.vue
@@ -34,7 +34,7 @@
 
 <script setup lang="ts">
 import { computed, ref, nextTick } from 'vue';
-import { FieldSpec } from '../../interfaces';
+import type { FieldSpec } from '../../interfaces';
 import { useModelStore, useSharedStore } from '../store/index';
 import { commonProps } from '../shared/helpers';
 
