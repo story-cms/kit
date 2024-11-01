7c0425ccf04b4afed7546b60ba65d4c20705f3d9
Timothy Koech <timosville@gmail.com>
2024-11-01 10:16:10 +0300

    feat: add animation field

---
+++ src/frontend/fields/animation-field.story.vue
@@ -0,0 +1,130 @@
+<template>
+  <Story id="animation-field" title="Animation Field" group="widgets">
+    <Variant title="Model with Attachment" :setup-app="loadData">
+      <AnimationField :field="spec" />
+      <ModelControl :model="objectModel" />
+    </Variant>
+
+    <Variant title="Model without attachment" :setup-app="loadData">
+      <AnimationField :field="spec" />
+      <ModelControl :model="modelBlankAnimation" />
+    </Variant>
+
+    <Variant title="Non Animation Upload" :setup-app="loadData">
+      <AnimationField :field="nonAnimationSpec" />
+      <ModelControl :model="modelBlankAnimation" />
+    </Variant>
+
+    <Variant title="Model with file attributes" :setup-app="loadData">
+      <AnimationField :field="fileAttributesSpec" />
+    </Variant>
+
+    <Variant title="Error" :setup-app="loadData">
+      <AnimationField
+        :field="{
+          name: 'success',
+          label: 'Success celebration',
+          widget: 'animation',
+        }"
+      />
+      <template #controls>
+        <ErrorControl :errors="objectErrors" />
+      </template>
+    </Variant>
+
+    <Variant title="Read Only" :setup-app="loadData">
+      <AnimationField
+        :field="{
+          name: 'success',
+          label: 'Success celebration (readonly)',
+          widget: 'animation',
+        }"
+        :is-read-only="true"
+      />
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import AnimationField from './animation-field.vue';
+import ErrorControl from './helpers/error-control.vue';
+import ModelControl from './helpers/model-control.vue';
+import { useModelStore, useSharedStore } from '../store';
+import type { StoryHandler } from '../shared/helpers';
+
+const loadData: StoryHandler = ({ variant }): void => {
+  const store = useModelStore();
+  const shared = useSharedStore();
+  if (variant?.title === 'Model without attachment') {
+    store.model = modelBlankAnimation;
+    return;
+  }
+  if (variant?.title === 'Error') {
+    shared.errors = objectErrors;
+    return;
+  }
+
+  if (variant?.title === 'Read Only') {
+    store.setSource({
+      ...objectModel,
+      success:
+        'https://res.cloudinary.com/onesheep/raw/upload/v1685641667/cmsplayground/fnu2m4ogxi9wdhi91iqi.riv',
+    });
+    return;
+  }
+  store.model = objectModel;
+};
+
+const provider = {
+  defaultPreset: 'cmsplayground',
+  cloudName: 'onesheep',
+};
+
+const objectModel = {
+  name: 'John',
+  age: 30,
+  success:
+    'https://res.cloudinary.com/onesheep/raw/upload/v1685641667/cmsplayground/fnu2m4ogxi9wdhi91iqi.riv',
+};
+
+const modelBlankAnimation = {
+  ...objectModel,
+  success: '',
+};
+
+const objectErrors = {
+  'bundle.name': ['required validation failed'],
+  'bundle.notes': ['required validation failed'],
+  'bundle.success': ['required validation failed'],
+};
+
+const spec = {
+  name: 'success',
+  label: 'Success celebration',
+  widget: 'animation',
+  uploadPreset: 'cmsplayground',
+  provider: provider,
+};
+
+const fileAttributesSpec = {
+  name: 'avatar',
+  label: 'Success celebration',
+  widget: 'animation',
+  uploadPreset: 'cmsplayground',
+  provider: provider,
+  description: 'RIV only',
+  extensions: ['.riv'],
+  maxSize: 2000000,
+};
+
+const nonAnimationSpec = {
+  name: 'avatar',
+  label: 'Success celebration',
+  widget: 'animation',
+  uploadPreset: 'cmsplayground',
+  provider: provider,
+  description: 'SVG, PNG up to 2MB',
+  extensions: ['.svg', '.png'],
+  maxSize: 2000000,
+};
+</script>
---
+++ src/frontend/fields/animation-field.vue
@@ -0,0 +1,62 @@
+<template>
+  <AttachmentField
+    v-bind="props"
+    :url="modelValue"
+    :errors="errors"
+    :host-service="host"
+    @delete="onDelete"
+    @attached="onAttached"
+  >
+    <RivePlayer :url="modelValue" />
+  </AttachmentField>
+</template>
+
+<script setup lang="ts">
+import { computed, ref, nextTick } from 'vue';
+import type { FieldSpec } from '../../interfaces';
+import { useModelStore, useSharedStore } from '../store';
+import { commonProps } from '../shared/helpers';
+import AttachmentField from './attachments/attachment-field.vue';
+import CloudinaryService from './attachments/cloudinary-service';
+import type { AttachmentModel } from './attachments/types';
+import RivePlayer from './attachments/rive-player.vue';
+
+const props = defineProps({
+  ...commonProps,
+});
+
+const model = useModelStore();
+const shared = useSharedStore();
+
+const field = computed(() => props.field as FieldSpec);
+const fieldPath = computed(() => {
+  if (props.rootPath === undefined) return field.value.name;
+  return `${props.rootPath}.${field.value.name}`;
+});
+
+const modelValue = props.isReadOnly
+  ? ref(model.getSourceField(fieldPath.value, '') as string)
+  : ref(model.getField(fieldPath.value, ''));
+
+const host = new CloudinaryService(field.value, '/raw/upload');
+
+model.$subscribe(() => {
+  if (props.isReadOnly) return;
+
+  nextTick().then(() => {
+    modelValue.value = model.getField(fieldPath.value, '') as string;
+  });
+});
+const errors = computed(() => shared.errorMessages(fieldPath.value));
+
+const onDelete = () => {
+  model.setField(fieldPath.value, '');
+  nextTick().then(() => {
+    modelValue.value = model.getField(fieldPath.value, '') as string;
+  });
+};
+
+const onAttached = (data: AttachmentModel) => {
+  model.setField(fieldPath.value, data.url);
+};
+</script>
