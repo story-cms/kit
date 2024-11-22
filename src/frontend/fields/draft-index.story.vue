eead46384379a813dc590424d44f2e7315726972
Timothy Koech <timosville@gmail.com>
2024-11-22 15:27:13 +0300

    refactor: add draft and translation index stories; create mobile app preview component

---
+++ src/frontend/fields/draft-index.story.vue
@@ -0,0 +1,61 @@
+<template>
+  <Story title="Draft Page" group="draft">
+    <Variant title="Index">
+      <DraftIndex
+        :draft="draft"
+        :bundle="{
+          id: 1,
+          number: 1,
+          status: 'submitted',
+          updatedAt: '2021-08-10T14:48:00.000000Z',
+          createdAt: '2021-08-10T14:48:00.000000Z',
+        }"
+        :spec="{
+          name: 'John',
+          chapterLimit: 21,
+          fields: fields,
+        }"
+        :last-published="'2021-10-10T14:48:00.000000Z'"
+        :providers="{}"
+        :errors="sharedProps.errors"
+        :meta="sharedProps.meta"
+        :user="sharedProps.user"
+        :language="sharedProps.language"
+        :languages="sharedProps.languages"
+        :story-name="'John'"
+        :stories="['John', 'Acts']"
+      />
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import DraftIndex from './draft-index.vue';
+import { listInListSpec, sharedProps } from '../test/mocks';
+
+const fields = [
+  { name: 'title', label: 'Title', widget: 'string' },
+  { ...listInListSpec },
+  { name: 'image', label: 'Image', widget: 'image' },
+  { name: 'body', label: 'Body', widget: 'markdown' },
+  {
+    name: 'nested',
+    label: 'Nested',
+    widget: 'object',
+    fields: [
+      { name: 'title', label: 'Title', widget: 'string' },
+      { name: 'description', label: 'Description', widget: 'string' },
+      { name: 'image', label: 'Image', widget: 'image' },
+      { name: 'body', label: 'Body', widget: 'markdown' },
+    ],
+  },
+];
+
+const draft = {
+  id: 1,
+  number: 1,
+  status: 'submitted',
+  updatedAt: '2021-08-10T14:48:00.000000Z',
+  createdAt: '2021-08-10T14:48:00.000000Z',
+};
+</script>
---
+++ src/frontend/fields/draft-index.vue
@@ -0,0 +1,223 @@
+<template>
+  <AppLayout>
+    <ContentHeader
+      :title="chapterTitle"
+      @delete="deleteDraft"
+      @info="info"
+      @app-preview="appPreview"
+    >
+      <WorkflowButtons @request-change="reject" @publish="publish" @submit="submit" />
+    </ContentHeader>
+
+    <div
+      class="container relative px-3 mx-auto"
+      :class="{
+        'grid grid-cols-[1fr,_416px] gap-x-8 ': isLargeScreen,
+        'mx-auto grid max-w-[1080px] grid-cols-[1fr] ':
+          !isLargeScreen || (!showMetaBox && !showAppPreview),
+      }"
+    >
+      <form :dir="shared.isRtl ? 'rtl' : 'ltr'" class="space-y-8">
+        <div v-for="(item, index) in drafts.story.fields" :key="index">
+          <component :is="widgetFor(index)" :field="item" :is-nested="false" />
+        </div>
+      </form>
+
+      <div
+        :class="{
+          'right-4': !isLargeScreen,
+          'absolute block': shared.isIntersecting,
+          'fixed right-4 top-24': !shared.isIntersecting && !isLargeScreen,
+          'sticky top-24  [align-self:start]': isLargeScreen,
+        }"
+      >
+        <section v-if="showMetaBox">
+          <MetaBox
+            :created-at="props.draft.createdAt"
+            :updated-at="props.draft.updatedAt"
+            :story-type="props.meta.storyType"
+            :chapter-type="metaChapter"
+            :published-when="publishedWhen"
+            :is-floating="!isLargeScreen"
+            @close="showMetaBox = false"
+          />
+        </section>
+        <section v-if="meta.hasAppPreview && showAppPreview" class="mt-6">
+          <MobileAppPreview
+            v-if="bundle"
+            :is-floating="!isLargeScreen"
+            :bundle="bundle"
+            :number="props.draft.number"
+            class="mt-2"
+            @close="showAppPreview = false"
+          />
+        </section>
+      </div>
+    </div>
+  </AppLayout>
+</template>
+
+<script setup lang="ts">
+import { computed, ref, onMounted, watch } from 'vue';
+import AppLayout from '../shared/app-layout.vue';
+import ContentHeader from '../shared/content-header.vue';
+import MetaBox from '../shared/meta-box.vue';
+import { router } from '@inertiajs/vue3';
+import type { Errors } from '@inertiajs/core';
+import { padZero, debounce, formatDate, safeChapterTitle } from '../shared/helpers';
+import type {
+  FieldSpec,
+  DraftEditProps,
+  SharedPageProps,
+  ResponseStatus,
+} from '../../interfaces';
+import { useDraftsStore, useModelStore, useSharedStore, useWidgetsStore } from '../store';
+import MobileAppPreview from './mobile-app-preview.vue';
+import WorkflowButtons from './workflow-buttons.vue';
+
+const props = defineProps<DraftEditProps & SharedPageProps>();
+
+const model = useModelStore();
+model.setFromProps(props);
+const shared = useSharedStore();
+shared.setFromProps(props);
+const drafts = useDraftsStore();
+drafts.setFromProps(props);
+const widgets = useWidgetsStore();
+widgets.setProviders(props.providers);
+
+let isSettingErrors = false;
+
+type postType = { feedback: string | undefined; bundle: any };
+
+const getPayload = (): postType => {
+  return {
+    feedback: '',
+    bundle: model.model,
+  };
+};
+
+const defaultTitle = computed(() => {
+  return `New ${props.meta.chapterType}`;
+});
+
+const title = ref(props.bundle.title);
+
+const chapterTitle = computed(() => {
+  if (title.value === defaultTitle.value) return defaultTitle.value;
+  return (
+    safeChapterTitle(title.value, props.storyName, props.draft.number) ??
+    defaultTitle.value
+  );
+});
+
+// actions
+const onSuccess = (message?: string) => {
+  widgets.setIsDirty(false);
+  if (!message) return;
+
+  shared.addMessage(ResponseStatus.Confirmation, message);
+};
+
+const onError = (errors: Errors, message: string) => {
+  widgets.setIsDirty(false);
+  console.log(errors);
+  isSettingErrors = true;
+  shared.setErrors(props.errors);
+  shared.addMessage(ResponseStatus.Failure, message);
+};
+
+const save = debounce(2000, () => {
+  router.post(`/draft/${props.draft.id}/save`, getPayload(), {
+    preserveScroll: true,
+    onSuccess: () => {
+      onSuccess();
+      if (props.user.role === 'admin') return;
+      drafts.setStatus('started');
+    },
+    onError: (e) => onError(e, `${props.meta.chapterType} not saved`),
+  });
+});
+
+const deleteDraft = () => {
+  router.delete(`/draft/${props.draft.id}`, {
+    onSuccess: () => onSuccess('Draft successfully deleted'),
+    onError: (e) => onError(e, 'Error deleting draft'),
+  });
+};
+
+const submit = () => {
+  router.post(`/draft/${props.draft.id}/submit`, getPayload(), {
+    onSuccess: () => onSuccess(`${props.meta.chapterType} submitted for review`),
+    onError: (e) =>
+      onError(e, 'Draft not submitted. Please review and correct any errors.'),
+  });
+};
+
+const publish = () => {
+  widgets.setIsDirty(true);
+  router.post(`/draft/${props.draft.id}/publish`, getPayload(), {
+    onSuccess: () => onSuccess(`${props.meta.chapterType} published successfully`),
+    onError: (e) =>
+      onError(
+        e,
+        `${props.meta.chapterType} not published. Please review and correct any errors.`,
+      ),
+  });
+};
+
+const reject = () => {
+  router.post(`/draft/${props.draft.id}/reject`, getPayload(), {
+    onSuccess: () => onSuccess('Draft sent back for fixing'),
+    onError: (e) => onError(e, 'Error sending draft back'),
+  });
+};
+
+const showMetaBox = ref(true);
+const showAppPreview = ref(true);
+
+const isLargeScreen = computed(() => {
+  return shared.isLargeScreen;
+});
+
+watch([showMetaBox, showAppPreview, isLargeScreen], ([a, b, c]) => {
+  if (c) {
+    showMetaBox.value = a;
+    showAppPreview.value = b;
+  }
+});
+
+const info = () => {
+  showMetaBox.value = !showMetaBox.value;
+};
+
+const appPreview = () => {
+  showAppPreview.value = !showAppPreview.value;
+};
+
+const publishedWhen = computed(() => {
+  return props.lastPublished == '' ? 'Unpublished' : formatDate(props.lastPublished);
+});
+
+const metaChapter = computed(
+  () => `${padZero(props.draft.number)} of ${padZero(props.spec.chapterLimit)}`,
+);
+
+onMounted(() => {
+  model.$subscribe(() => {
+    if (isSettingErrors) {
+      isSettingErrors = false;
+      return;
+    }
+    widgets.setIsDirty(true);
+    save();
+    title.value = model.getField('title', defaultTitle.value);
+    console.info('title', title.value);
+  });
+});
+
+const widgetFor = (key: number) => {
+  const widget = (props.spec.fields as FieldSpec[])[key].widget;
+  return widgets.picker(widget);
+};
+</script>
