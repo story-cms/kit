b0024f47d627e0e6ae40480707c9b675c2df7576
Timothy Koech <timosville@gmail.com>
2024-11-27 11:59:28 +0300

    feat: add chapters

---
+++ src/frontend/chapters/chapter-preview.vue
@@ -0,0 +1,127 @@
+<template>
+  <AppLayout>
+    <ContentHeader :title="chapterTitle" @info="info" @app-preview="appPreview">
+      <button
+        type="button"
+        class="w-32 rounded-[38px] border border-blue-500 bg-blue-500 px-[15px] py-[9px] text-sm/5 font-medium text-white shadow"
+        @click.prevent="edit"
+      >
+        Edit
+      </button>
+    </ContentHeader>
+
+    <div
+      class="container relative px-3 mx-auto"
+      :class="{
+        'grid grid-cols-[1fr,_416px] gap-x-8 ': isLargeScreen,
+        'mx-auto grid max-w-[1080px] grid-cols-[1fr] ':
+          !isLargeScreen || (!showMetaBox && !showAppPreview),
+      }"
+    >
+      <!-- eslint-disable vue/no-v-html -->
+      <div class="p-8 -ml-8 bg-white shadow-sm border-s" v-html="bundleView"></div>
+
+      <div
+        :class="{
+          'right-4': !isLargeScreen,
+          'absolute block': shared.isIntersecting,
+          'fixed right-4 top-24': !shared.isIntersecting && !isLargeScreen,
+          'sticky top-24  [align-self:start]': isLargeScreen,
+        }"
+      >
+        <section v-if="showMetaBox">
+          <MetaBox
+            :created-at="props.chapter.createdAt"
+            :updated-at="props.chapter.updatedAt"
+            :story-type="props.meta.storyType"
+            :chapter-type="metaChapter"
+            :published-when="publishedWhen"
+            :is-floating="!isLargeScreen"
+            :story-name="storyName"
+            @close="showMetaBox = false"
+          />
+        </section>
+        <section v-if="meta.hasAppPreview && showAppPreview" class="mt-6">
+          <MobileAppPreview
+            v-if="bundle"
+            :is-floating="!isLargeScreen"
+            :bundle="bundle"
+            :number="props.chapter.number"
+            class="mt-2"
+            @close="showAppPreview = false"
+          />
+        </section>
+      </div>
+    </div>
+  </AppLayout>
+</template>
+
+<script setup lang="ts">
+import { computed, ref, watch, onMounted } from 'vue';
+import AppLayout from '../shared/app-layout.vue';
+import ContentHeader from '../shared/content-header.vue';
+import MetaBox from '../shared/meta-box.vue';
+import MobileAppPreview from '../fields/mobile-app-preview.vue';
+import { formatDate, padZero, safeChapterTitle } from '../shared/helpers';
+import type { PreviewProps, SharedPageProps } from '../../interfaces';
+import { useSharedStore } from '../store';
+
+const props = defineProps<PreviewProps & SharedPageProps>();
+
+const shared = useSharedStore();
+shared.setFromProps(props);
+
+const chapterTitle =
+  safeChapterTitle(props.title, props.storyName, props.chapter.number) ??
+  `New ${props.meta.chapterType}`;
+
+const showMetaBox = ref(true);
+const showAppPreview = ref(true);
+
+const isLargeScreen = computed(() => {
+  return shared.isLargeScreen;
+});
+
+watch([showMetaBox, showAppPreview, isLargeScreen], ([a, b, c]) => {
+  if (c) {
+    showMetaBox.value = a;
+    showAppPreview.value = b;
+  }
+});
+
+const publishedWhen = computed(() => {
+  return props.chapter.updatedAt === ''
+    ? 'Unpublished'
+    : formatDate(props.chapter.updatedAt);
+});
+
+const metaChapter = computed(
+  () => `${padZero(props.chapter.number)} of ${padZero(props.chapterLimit)}`,
+);
+
+const info = () => {
+  console.info('info');
+  showMetaBox.value = !showMetaBox.value;
+};
+
+const appPreview = () => {
+  console.info('app-preview');
+  showAppPreview.value = !showAppPreview.value;
+};
+
+// const close = () => {
+//   window.location.href = '/';
+// };
+
+const edit = () => {
+  window.location.href = `/draft/${props.chapter.number}/edit`;
+};
+
+onMounted(() => {
+  if (shared.meta.hasAppPreview) {
+    showAppPreview.value = true;
+  } else {
+    showAppPreview.value = false;
+  }
+});
+</script>
---
+++ src/frontend/chapters/index-card.vue
@@ -0,0 +1,112 @@
+<template>
+  <button
+    type="button"
+    class="grid overflow-hidden rounded-lg bg-white shadow [grid-template-areas:'image'_'content']"
+    :class="{
+      'grid-cols-1 grid-rows-[186px_1fr]': showImage,
+      'w-full grid-cols-2': isList,
+    }"
+    @click="emit('tap', item)"
+  >
+    <div v-if="showImage" class="overflow-hidden [grid-area:image]">
+      <img
+        class="h-[186px] w-full object-cover object-center"
+        :src="imgUrl"
+        :alt="title"
+      />
+    </div>
+    <div
+      class="p-6"
+      :class="{
+        '[grid-area:content]': !isList,
+        'col-span-full grid grid-cols-[subgrid]': isList,
+      }"
+    >
+      <div
+        class="grid grid-rows-[min-content_min-content] place-content-start text-left"
+        :class="{
+          'mb-3': !isList,
+        }"
+      >
+        <h4 class="mb-1 font-bold text-gray-900">
+          {{ chapterName }} {{ chapterNumber }}
+        </h4>
+        <p class="text-gray-500" :class="{ 'text-sm': isList, 'text-xs': !isList }">
+          {{ title }}
+        </p>
+      </div>
+      <div
+        v-if="tags.length > 0"
+        :class="{
+          'col-start-2 self-center justify-self-end': isList,
+          'flex items-center space-x-4': !isList,
+        }"
+      >
+        <StatusTag v-for="tag in tags" :key="tag" :tag="tag" />
+      </div>
+    </div>
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import { computed } from 'vue';
+import StatusTag from './status-tag.vue';
+import type { IndexReadyItem } from '../../interfaces';
+import { padZero } from '../shared/helpers';
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<IndexReadyItem>,
+    required: true,
+  },
+  isList: {
+    type: Boolean,
+    required: true,
+  },
+  scope: {
+    type: String,
+    required: true,
+  },
+  placeholderImage: {
+    type: String,
+    default: '',
+    required: false,
+  },
+  chapterName: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['tap']);
+
+const imgUrl = computed(() => props.item.imageUrl || props.placeholderImage);
+
+const title = computed(
+  () => props.item.title || `${props.chapterName} ${props.item.number}`,
+);
+
+const showImage = computed((): boolean => {
+  if (props.isList) {
+    return false;
+  }
+  return !!(props.item.imageUrl || props.placeholderImage);
+});
+
+const chapterNumber = computed(() => padZero(props.item.number));
+
+const tags = computed(() => {
+  if (props.scope === 'Live') {
+    if (props.item.tags.includes('Draft')) {
+      return ['Changed'];
+    }
+  } else {
+    if (props.item.tags.includes('Live')) {
+      return ['Changes'];
+    }
+    return ['New'];
+  }
+  return [];
+});
+</script>
