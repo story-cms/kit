97c11f593060865aeaac34ee1a1fb057ddc02205
Timothy Koech <timosville@gmail.com>
2024-12-17 15:39:33 +0300

    feat: add new field components and update exports in index file

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -8,10 +8,8 @@ import DashboardIndex from './dashboard/dashboard-index.vue';
 import Login from './auth/login-index.vue';
 import ForgotPassword from './auth/forgot-password.vue';
 import ResetPassword from './auth/reset-password.vue';
-
 import IndexCard from './chapters/index-card.vue';
 import StatusTag from './chapters/status-tag.vue';
-
 import AnimationField from './fields/animation-field.vue';
 import AudioField from './fields/audio-field.vue';
 import ImageField from './fields/image-field.vue';
@@ -24,16 +22,18 @@ import ObjectField from './fields/object-field.vue';
 import PanelField from './fields/panel-field.vue';
 import ScriptureField from './fields/scripture-field.vue';
 import SelectField from './fields/select-field.vue';
-
 import IndexFilter from './shared/index-filter.vue';
-
 import AddItemButton from './shared/add-item-button.vue';
-
 import PublicLayout from './shared/public-layout.vue';
-
 import StickyHeader from './shared/sticky-header.vue';
-
 import PageIndexItem from './fields/pages/page-index-item.vue';
+import DraftIndex from './fields/draft-index.vue';
+import TranslationIndex from './fields/translation-index.vue';
+import PagesIndex from './fields/pages/pages-index.vue';
+import PagesEdit from './fields/pages/pages-edit.vue';
+import ChapterPreview from './chapters/chapter-preview.vue';
+import UiIndex from './fields/interface/ui-index.vue';
+import UiPage from './fields/interface/ui-page.vue';
 
 export { createInertiaApp, usePage, router } from '@inertiajs/vue3';
 
@@ -69,4 +69,11 @@ export {
   PublicLayout,
   StickyHeader,
   PageIndexItem,
+  DraftIndex,
+  TranslationIndex,
+  PagesIndex,
+  PagesEdit,
+  ChapterPreview,
+  UiIndex,
+  UiPage,
 };
