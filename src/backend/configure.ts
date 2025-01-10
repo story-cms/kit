522bc50139cd71282b636ef5f88729155cb273b9
Timothy Koech <timosville@gmail.com>
2025-01-10 09:30:56 +0300

    feat: add page validation and update PagesController to use new validator

--- src/backend/configure.ts
+++ src/backend/configure.ts
@@ -115,6 +115,7 @@ export async function configure(command: Configure) {
   await codemods.makeUsingStub(stubsRoot, 'validators/user.stub', {});
   await codemods.makeUsingStub(stubsRoot, 'validators/auth.stub', {});
   await codemods.makeUsingStub(stubsRoot, 'validators/bundle.stub', {});
+  await codemods.makeUsingStub(stubsRoot, 'validators/pages.stub', {});
   await codemods.makeUsingStub(stubsRoot, 'commands/make_user.stub', {});
   await addMigrations(command, codemods);
 
