de3f0930253f82ff2b73d31757c486dcbea29092
Timothy Koech <timosville@gmail.com>
2025-01-16 10:31:08 +0300

    feat: add admin service stub and integrate into configuration

--- src/backend/configure.ts
+++ src/backend/configure.ts
@@ -117,6 +117,7 @@ export async function configure(command: Configure) {
   await codemods.makeUsingStub(stubsRoot, 'services/chapter_service.stub', {});
   await codemods.makeUsingStub(stubsRoot, 'services/draft_service.stub', {});
   await codemods.makeUsingStub(stubsRoot, 'services/page_service.stub', {});
+  await codemods.makeUsingStub(stubsRoot, 'services/admin_service.stub', {});
   await codemods.makeUsingStub(stubsRoot, 'validators/user.stub', {});
   await codemods.makeUsingStub(stubsRoot, 'validators/auth.stub', {});
   await codemods.makeUsingStub(stubsRoot, 'validators/bundle.stub', {});
