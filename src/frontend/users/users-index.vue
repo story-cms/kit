56499ffe3e0ec315dbb330385e80cc41f474d67e
Timothy Koech <timosville@gmail.com>
2025-05-09 16:58:41 +0300

    wip

--- src/frontend/users/users-index.vue
+++ src/frontend/users/users-index.vue
@@ -16,7 +16,7 @@
       </ContentHeader>
     </template>
     <div>
-      <div class="mt-8 flow-root">
+      <section class="mt-8 flow-root">
         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div class="overflow-hidden shadow ring-1 ring-black/5 sm:rounded-lg">
@@ -53,110 +53,86 @@
                   </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200 bg-white">
-                  <tr v-for="person in people" :key="person.email">
-                    <UserRow :user="person" />
+                  <tr v-for="user in users" :key="user.id">
+                    <UserRow :user="user" />
                   </tr>
                 </tbody>
               </table>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div>
-      <h2 class="mb-4 text-2xl font-bold leading-7 text-black">Manage Users</h2>
-      <div v-if="otherError" class="py-4 text-error">
-        {{ otherError }}
-      </div>
-      <section v-if="formMode == 'hidden'">
-        <ul class="md:w-1/3">
-          <li
-            v-for="user in users"
-            :key="user.id"
-            class="flex justify-between border-b border-gray-300 py-2 text-xl"
-          >
-            <div class="cursor-pointer" @click="focus(user)">
-              {{ user.name }} &lt;{{ user.email }}&gt;
-            </div>
-            <button
-              v-if="user.id != shared.user.id"
-              type="button"
-              class="cursor-pointer"
-              @click="deleteUser(user)"
-            >
-              <icon name="trash" class="h-10 w-10" />
-            </button>
-          </li>
-        </ul>
-        <div class="my-8">
-          <button type="button" class="btn btn-blue w-32" @click="onAdd()">
-            Add User
-          </button>
-        </div>
       </section>
+      <section class="fixed inset-0 z-30 bg-gray-600 bg-opacity-60">
+        <div class="mx-auto flex h-full max-w-lg flex-col items-center justify-center">
+          <form
+            class="relative w-full min-w-full rounded-lg bg-white px-20 pb-[75px] pt-[90px]"
+            @submit.prevent="submit"
+          >
+            <div class="my-2">
+              <label class="input-label" for="name">Name:</label>
+              <div class="mt-[2px] pt-1">
+                <input id="name" v-model="form.name" class="input-field" />
+                <p v-if="form.errors.name" class="text-sm text-error">
+                  {{ form.errors.name[0] }}
+                </p>
+              </div>
+            </div>
 
-      <section v-if="formMode != 'hidden'" class="md:w-1/3">
-        <form @submit.prevent="submit">
-          <div class="my-2">
-            <label class="input-label" for="name">Name:</label>
-            <div class="mt-[2px] pt-1">
-              <input id="name" v-model="form.name" class="input-field" />
-              <p v-if="form.errors.name" class="text-sm text-error">
-                {{ form.errors.name[0] }}
+            <div class="my-2">
+              <label class="input-label" for="email">Email:</label>
+              <div class="mt-[2px] pt-1">
+                <input id="email" v-model="form.email" class="input-field" />
+                <p v-if="form.errors.email" class="text-sm text-error">
+                  {{ form.errors.email[0] }}
+                </p>
+              </div>
+            </div>
+
+            <div v-if="focusId != shared.user.id" class="my-2">
+              <label class="input-label" for="role">Role:</label>
+              <select id="role" v-model="form.role" class="input-field">
+                <option
+                  v-for="role in roles"
+                  :key="role"
+                  :value="role"
+                  :selected="role == form.role"
+                >
+                  {{ role }}
+                </option>
+              </select>
+            </div>
+
+            <div class="my-2">
+              <label class="input-label" for="language">Language:</label>
+              <select id="language" v-model="form.language" class="input-field">
+                <option value="*" :selected="form.language === '*'">All Languages</option>
+                <option
+                  v-for="lang in shared.languages"
+                  :key="lang.locale"
+                  :value="lang.locale"
+                  :selected="lang.locale == form.language"
+                >
+                  {{ lang.language }}
+                </option>
+              </select>
+              <p v-if="form.errors.language" class="text-sm text-error">
+                {{ form.errors.language[0] }}
               </p>
             </div>
-          </div>
 
-          <div class="my-2">
-            <label class="input-label" for="email">Email:</label>
-            <div class="mt-[2px] pt-1">
-              <input id="email" v-model="form.email" class="input-field" />
-              <p v-if="form.errors.email" class="text-sm text-error">
-                {{ form.errors.email[0] }}
-              </p>
+            <div class="my-8 flex space-x-4">
+              <!-- eslint-disable vue/no-v-html -->
+              <button
+                class="btn btn-blue w-32"
+                type="submit"
+                v-html="submitLabel"
+              ></button>
+              <button class="btn w-32 bg-white" type="button" @click.prevent="onCancel()">
+                Cancel
+              </button>
             </div>
-          </div>
-
-          <div v-if="focusId != shared.user.id" class="my-2">
-            <label class="input-label" for="role">Role:</label>
-            <select id="role" v-model="form.role" class="input-field">
-              <option
-                v-for="role in roles"
-                :key="role"
-                :value="role"
-                :selected="role == form.role"
-              >
-                {{ role }}
-              </option>
-            </select>
-          </div>
-
-          <div class="my-2">
-            <label class="input-label" for="language">Language:</label>
-            <select id="language" v-model="form.language" class="input-field">
-              <option value="*" :selected="form.language === '*'">All Languages</option>
-              <option
-                v-for="lang in shared.languages"
-                :key="lang.locale"
-                :value="lang.locale"
-                :selected="lang.locale == form.language"
-              >
-                {{ lang.language }}
-              </option>
-            </select>
-            <p v-if="form.errors.language" class="text-sm text-error">
-              {{ form.errors.language[0] }}
-            </p>
-          </div>
-
-          <div class="my-8 flex space-x-4">
-            <!-- eslint-disable vue/no-v-html -->
-            <button class="btn btn-blue w-32" type="submit" v-html="submitLabel"></button>
-            <button class="btn w-32 bg-white" type="button" @click.prevent="onCancel()">
-              Cancel
-            </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </section>
     </div>
   </AppLayout>
@@ -169,7 +145,7 @@ import AppLayout from '../shared/app-layout.vue';
 import ContentHeader from '../shared/content-header.vue';
 import Icon from '../shared/icon.vue';
 import UserRow from './components/user-row.vue';
-import { SharedPageProps, UsersProps, UserMeta, type User } from '../../types';
+import { SharedPageProps, UsersProps, UserMeta } from '../../types';
 import { useSharedStore } from '../store';
 
 const emptyForm = {
@@ -261,85 +237,4 @@ const submit = () => {
 };
 
 const submitLabel = computed(() => (formMode.value === 'update' ? 'Update' : 'Add'));
-
-const people: User[] = [
-  {
-    id: 1,
-    name: 'Lindsay Walton',
-    initials: 'LW',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    isManager: false,
-    isAdmin: false,
-    language: 'en',
-    lastActivity: '2021-01-01',
-  },
-  // TODO: Add more users with different roles and names
-  {
-    id: 2,
-    name: 'John Doe',
-    initials: 'JD',
-    email: 'john.doe@example.com',
-    role: 'Admin',
-    isManager: false,
-    isAdmin: false,
-    language: 'en',
-    lastActivity: 'Yesterday',
-  },
-  {
-    id: 3,
-    name: 'Jane Smith',
-    initials: 'JS',
-    email: 'jane.smith@example.com',
-    role: 'Editor',
-    isManager: false,
-    isAdmin: false,
-    language: 'es',
-    lastActivity: 'Tuesday',
-  },
-  {
-    id: 4,
-    name: 'Alice Johnson',
-    initials: 'AJ',
-    email: 'alice.johnson@example.com',
-    role: 'Member',
-    isManager: false,
-    isAdmin: false,
-    language: 'es',
-    lastActivity: 'Yesterday',
-  },
-  {
-    id: 5,
-    name: 'Bob Brown',
-    initials: 'BB',
-    email: 'bob.brown@example.com',
-    role: 'Member',
-    isManager: false,
-    isAdmin: false,
-    language: 'en',
-    lastActivity: '7th April',
-  },
-  {
-    id: 6,
-    name: 'Charlie Davis',
-    initials: 'CD',
-    email: 'charlie.davis@example.com',
-    role: 'Member',
-    isManager: false,
-    isAdmin: false,
-    language: 'es',
-    lastActivity: 'Monday',
-  },
-  {
-    id: 7,
-    name: 'Diana Evans',
-    initials: 'DE',
-    email: 'diana.evans@example.com',
-    role: 'Member',
-    isManager: false,
-    isAdmin: false,
-    language: '*',
-    lastActivity: 'Friday',
-  },
-];
 </script>
