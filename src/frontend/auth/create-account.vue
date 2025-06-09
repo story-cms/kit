ab873d6b3faca436c97e55d4fd9fdfc9424852e7
Timothy Koech <timosville@gmail.com>
2025-06-09 13:04:36 +0300

    refactor: match fields to spec

--- src/frontend/auth/create-account.vue
+++ src/frontend/auth/create-account.vue
@@ -1,85 +1,90 @@
 <template>
   <PublicLayout>
     <template #logo>
-      <img :src="meta.logo" alt="Logo" class="w-auto h-12" />
+      <img :src="meta.logo" alt="Logo" class="h-12 w-auto" />
     </template>
 
     <div>
-      <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900">
+      <h2 class="mt-6 text-center font-bold leading-9 text-gray-900">
         Create your account
       </h2>
-      <p v-if="success" class="mt-2 text-sm text-center text-green-500">
+      <p v-if="success" class="mt-2 text-center text-sm text-green-500">
         Account created successfully
       </p>
       <a href="/login"
         ><button
           v-if="success"
           type="submit"
-          class="relative flex justify-center w-full px-4 py-2 mt-4 text-sm font-medium text-white border border-transparent rounded-md group bg-gray-700/60 hover:bg-gray-700/70 focus:outline-none focus:ring-2 focus:ring-gray-700/50 focus:ring-offset-2"
+          class="group relative mt-4 flex w-full justify-center rounded-md border border-transparent bg-gray-700/60 px-4 py-2 text-sm font-medium text-white hover:bg-gray-700/70 focus:outline-none focus:ring-2 focus:ring-gray-700/50 focus:ring-offset-2"
         >
           Login
         </button></a
       >
 
       <div v-if="errors">
-        <p v-if="errors.password" class="mt-2 text-sm text-center text-error">
+        <p v-if="errors.password" class="mt-2 text-center text-sm text-error">
           {{ errors.password[0] }}
         </p>
 
-        <p v-if="errors.token" class="mt-2 text-sm text-center text-error">
+        <p v-if="errors.token" class="mt-2 text-center text-sm text-error">
           {{ errors.token }}
         </p>
       </div>
     </div>
     <form v-if="!success" class="mt-8 space-y-6" @submit.prevent="submit">
-
-    
-      <div class="-space-y-px rounded-md shadow-sm">
+      <div class="flex flex-col gap-y-6">
         <div>
-          <label for="email-address" class="sr-only">Email address</label>
-          <input
-            id="email-address"
-            v-model="form.email"
-            name="email"
-            type="email"
-            autocomplete="email"
-            required
-            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:z-10 focus:border-gray-700/50 focus:outline-none focus:ring-gray-700/50 sm:text-sm"
-            placeholder="Email address"
-          />
+          <label for="email" class="block text-sm font-medium text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-1">
+            <input
+              id="email"
+              type="email"
+              name="email"
+              autocomplete="email"
+              required
+              readonly
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-sm font-medium leading-5 text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400"
+            />
+          </div>
         </div>
         <div>
-          <label for="password" class="sr-only">New Password</label>
-          <input
-            id="new-password"
-            v-model="form.password"
-            name="password"
-            type="password"
-            autocomplete="new-password"
-            required
-            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none focus:z-10 focus:border-gray-700/50 focus:outline-none focus:ring-gray-700/50 sm:text-sm"
-            placeholder="New Password"
-          />
+          <label for="email" class="block text-sm font-medium text-gray-900"
+            >Password</label
+          >
+          <div class="mt-1">
+            <input
+              id="email"
+              type="email"
+              name="email"
+              autocomplete="email"
+              required=""
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-sm font-medium leading-5 text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            />
+          </div>
         </div>
         <div>
-          <label for="password" class="sr-only">Confirm New Password</label>
-          <input
-            id="confirm-new-password"
-            v-model="form.confirmPassword"
-            name="confirmPassword"
-            type="password"
-            autocomplete="confirm-new-password"
-            required
-            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none focus:ring-gray-700-500 focus:border-gray-700-500 rounded-b-md focus:z-10 focus:outline-none sm:text-sm"
-            placeholder="Confirm New Password"
-          />
+          <label for="email" class="block text-sm font-medium text-gray-900"
+            >Confirm Password</label
+          >
+          <div class="mt-1">
+            <input
+              id="email"
+              type="email"
+              name="email"
+              autocomplete="email"
+              required=""
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-sm font-medium leading-5 text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+            />
+          </div>
         </div>
       </div>
 
       <div>
         <button
           type="submit"
-          class="relative flex justify-center px-4 py-2 mx-auto text-sm font-medium text-white bg-blue-500 border border-transparent rounded-full group hover:bg-gray-700/70 focus:outline-none focus:ring-2 focus:ring-gray-700/50 focus:ring-offset-2"
+          class="group relative mx-auto flex justify-center rounded-full border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-gray-700/70 focus:outline-none focus:ring-2 focus:ring-gray-700/50 focus:ring-offset-2"
         >
           Reset Password
         </button>
@@ -89,8 +94,8 @@
 </template>
 
 <script setup lang="ts">
-import { PropType, ref } from 'vue';
-import { useForm } from '@inertiajs/vue3';
+import { PropType } from 'vue';
+
 import PublicLayout from '../shared/public-layout.vue';
 import { Meta } from '../../types';
 
@@ -105,20 +110,4 @@ defineProps({
     required: true,
   },
 });
-
-const success = ref(false);
-const form = useForm({
-  email: null,
-  password: null,
-  confirmPassword: null,
-});
-
-function submit() {
-  const token = window.location.href.split('reset-password/')[1];
-  form.post(`/reset-password/${token}`, {
-    onSuccess() {
-      success.value = true;
-    },
-  });
-}
 </script>
