7f757a9064d0014d3c777d23d9a07f5d3cbfab87
Jannie Theunissen <jannie@scoutredeem.co>
2025-06-24 11:22:43 +0200

    refactor: move models to package

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,3 +3,4 @@ export * from '../types.js';
 export { configure } from './configure.js';
 export { stubsRoot } from './stubs/main.js';
 export { defineConfig } from './define_config.js';
+export { default as Activity } from './models/activity.js';
---
+++ src/backend/models/activity.ts
@@ -0,0 +1,19 @@
+import { DateTime } from 'luxon';
+import { BaseModel, column } from '@adonisjs/lucid/orm';
+
+export default class Activity extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number;
+
+  @column()
+  declare userId: number;
+
+  @column()
+  declare locale: string;
+
+  @column()
+  declare action: string;
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime;
+}
