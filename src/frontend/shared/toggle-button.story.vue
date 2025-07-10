4da0bdd1224f5323537fae897faeb182b588b818
Jannie Theunissen <jannie@scoutredeem.co>
2025-07-10 15:44:31 +0200

    feat: toggle button

---
+++ src/frontend/shared/toggle-button.story.vue
@@ -0,0 +1,19 @@
+<template>
+  <Story title="Toggle Button" group="shared" :layout="{ type: 'grid', width: 300 }">
+    <Variant title="Default">
+      <ToggleButton
+        icon-on="dots-horizontal"
+        icon-off="sort"
+        label="Toggle"
+        :is-on="isOn"
+        @toggle="isOn = !isOn"
+      />
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ToggleButton from './toggle-button.vue';
+const isOn = ref(false);
+</script>
---
+++ src/frontend/shared/toggle-button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    type="button"
+    class="'bg-white inline-flex items-center gap-x-2 rounded-full px-3.5 py-2.5 text-sm font-medium shadow-sm ring-1 ring-inset focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    @click="$emit('toggle')"
+  >
+    <Icon :name="icon" />
+    {{ label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import Icon from './icon.vue';
+
+const props = defineProps<{
+  iconOn: string;
+  iconOff: string;
+  label: string;
+  isOn: boolean;
+}>();
+
+defineEmits<{
+  (e: 'toggle'): void;
+}>();
+
+const icon = computed(() => {
+  return props.isOn ? props.iconOn : props.iconOff;
+});
+</script>
