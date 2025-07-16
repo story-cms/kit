d747cf494a0f60b9e0337d2cef88a6d6647b1a6c
Jannie Theunissen <jannie@scoutredeem.co>
2025-07-16 07:51:37 +0200

    fix: linting

--- src/frontend/dashboard/ring.story.vue
+++ src/frontend/dashboard/ring.story.vue
@@ -1,19 +1,37 @@
 <template>
   <Story title="Ring" group="dashboard" :layout="{ type: 'grid', width: 200 }">
     <Variant title="Started">
-      <Ring name="Deutsch" :done="50" :draft="20" :total="100" lastUpdated="2025-07-16" />
+      <Ring
+        name="Deutsch"
+        :done="50"
+        :draft="20"
+        :total="100"
+        last-updated="2025-07-16"
+      />
     </Variant>
 
     <Variant title="Not started">
-      <Ring name="Deutsch" :done="0" :draft="0" :total="110" lastUpdated="2025-07-16" />
+      <Ring name="Deutsch" :done="0" :draft="0" :total="110" last-updated="2025-07-16" />
     </Variant>
 
     <Variant title="Done">
-      <Ring name="Deutsch" :done="100" :draft="0" :total="100" lastUpdated="2025-07-16" />
+      <Ring
+        name="Deutsch"
+        :done="100"
+        :draft="0"
+        :total="100"
+        last-updated="2025-07-16"
+      />
     </Variant>
 
     <Variant title="Done not QAed">
-      <Ring name="Deutsch" :done="80" :draft="20" :total="100" lastUpdated="2025-07-16" />
+      <Ring
+        name="Deutsch"
+        :done="80"
+        :draft="20"
+        :total="100"
+        last-updated="2025-07-16"
+      />
     </Variant>
 
     <Variant title="Overdone">
@@ -22,12 +40,12 @@
         :done="100"
         :draft="50"
         :total="120"
-        lastUpdated="2025-07-16"
+        last-updated="2025-07-16"
       />
     </Variant>
 
     <Variant title="Error: Total is 0">
-      <Ring name="Deutsch" :done="0" :draft="0" :total="0" lastUpdated="2025-07-16" />
+      <Ring name="Deutsch" :done="0" :draft="0" :total="0" last-updated="2025-07-16" />
     </Variant>
 
     <Variant title="Error: data is garbage">
@@ -36,7 +54,7 @@
         done="something"
         :draft="50"
         :total="0"
-        lastUpdated="2025-07-16"
+        last-updated="2025-07-16"
       />
     </Variant>
   </Story>
