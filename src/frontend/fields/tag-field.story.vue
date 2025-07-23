9f99ff90aab67169f6cb75bb0f58411342b60e5f
Timothy Koech <timosville@gmail.com>
2025-07-23 14:53:07 +0300

    feat: enhance TagField story with additional variants and model control for better testing

--- src/frontend/fields/tag-field.story.vue
+++ src/frontend/fields/tag-field.story.vue
@@ -1,11 +1,43 @@
 <template>
   <Story title="Tag Field" group="widgets">
-    <Variant title="With model">
-      <TagField />
+    <Variant title="Default">
+      <TagField :field="{ name: 'tags', label: 'Tags', widget: 'tag' }" />
+    </Variant>
+    <Variant title="With model" :setup-app="loadData">
+      <TagField :field="{ name: 'tags', label: 'Tags', widget: 'tag' }" />
+      <ModelControl :model="objectModel" />
+    </Variant>
+    <Variant title="Read only" :setup-app="loadData">
+      <TagField
+        :field="{ name: 'tags', label: 'Tags', widget: 'tag' }"
+        :is-read-only="true"
+      />
+      <ModelControl :model="objectModel" />
     </Variant>
   </Story>
 </template>
 
 <script setup lang="ts">
 import TagField from './tag-field.vue';
+import { useModelStore, useSharedStore } from '../store';
+import type { StoryHandler } from '../shared/helpers';
+import ModelControl from '../test/model-control.vue';
+
+import { objectErrors } from '../../frontend/test/mocks';
+
+const objectModel = {
+  title: 'Draft',
+  tags: ['simul', 'justus', 'et', 'peccator'],
+};
+
+const loadData: StoryHandler = ({ variant }): void => {
+  const store = useModelStore();
+  const shared = useSharedStore();
+
+  if (variant?.title === 'With error') {
+    shared.errors = objectErrors;
+    return;
+  }
+  store.model = objectModel;
+};
 </script>
