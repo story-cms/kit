66d4a9937dc2734e72b771c246fb3711c775c138
Timothy Koech <timosville@gmail.com>
2025-07-28 14:21:14 +0300

    feat: add LabelButton and ListSwitcher components for enhanced UI interactions

---
+++ src/frontend/shared/list-switcher.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="emit('toggle')">
+      <Icon :name="iconName" />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@story-cms/kit/ui';
+import { computed } from 'vue';
+
+const props = defineProps<{
+  isList: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: 'toggle'): void;
+}>();
+
+const iconName = computed(() => {
+  return props.isList ? 'grid' : 'list';
+});
+</script>
