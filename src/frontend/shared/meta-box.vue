80fe61efc902debcea3b74aefb596e6ca6a0c1c5
Timothy Koech <timosville@gmail.com>
2025-07-28 14:21:22 +0300

    refactor: simplify meta-box component structure and improve data binding for primary and secondary items

--- src/frontend/shared/meta-box.vue
+++ src/frontend/shared/meta-box.vue
@@ -1,54 +1,30 @@
 <template>
-  <div class="rounded-lg border border-gray-100 font-['Inter']">
-    <section
-      class="grid grid-cols-1 px-4 py-2 text-xs font-semibold leading-4 text-gray-500 gap-y-2 bg-gray-50"
+  <div class="rounded-lg border bg-white font-['Inter'] text-gray-600 shadow">
+    <div
+      v-for="item in primary"
+      :key="item.label"
+      class="grid grid-cols-2 bg-gray-50 px-4 py-2"
     >
-      <div class="grid grid-cols-2">
-        <p>{{ props.storyType }}</p>
-        <span class="text-right place-self-end">{{ story }}</span>
-      </div>
-      <div class="grid grid-cols-2">
-        <p>Chapter</p>
-        <span class="text-right place-self-end">{{ props.chapterType }}</span>
-      </div>
-    </section>
-    <section
-      class="grid grid-cols-1 px-4 pt-2 pb-4 text-xs font-normal leading-4 text-gray-600 bg-white p gap-y-2"
+      <p class="text-sm font-semibold leading-5 text-gray-500">{{ item.label }}</p>
+      <span class="place-self-end text-right text-sm font-normal leading-5">{{
+        item.value
+      }}</span>
+    </div>
+    <div
+      v-for="item in secondary"
+      :key="item.label"
+      class="grid grid-cols-2 px-4 pb-2 text-gray-600"
     >
-      <div class="grid grid-cols-2">
-        <p>Created</p>
-        <span class="text-right place-self-end">{{ formatDate(props.createdAt) }}</span>
-      </div>
-      <div class="grid grid-cols-2">
-        <p>Auto-Saved</p>
-        <span class="text-right place-self-end">{{ formatDate(props.updatedAt) }}</span>
-      </div>
-      <div class="grid grid-cols-2">
-        <p>Last Published</p>
-        <span class="text-right place-self-end">{{ publishedWhen }}</span>
-      </div>
-    </section>
+      <p class="text-sm font-normal leading-5">{{ item.label }}</p>
+      <span class="place-self-end text-right text-sm font-normal leading-5">{{
+        item.value
+      }}</span>
+    </div>
   </div>
 </template>
-
 <script setup lang="ts">
-import { computed } from 'vue';
-import type { ChapterMeta, Meta } from '../../types';
-import { formatDate } from './helpers';
-import { useDraftsStore } from '../store';
-
-interface Props {
-  storyType: Meta['storyType'];
-  chapterType: Meta['chapterType'];
-  createdAt: ChapterMeta['createdAt'];
-  updatedAt: ChapterMeta['updatedAt'];
-  publishedWhen: string;
-  isFloating?: boolean;
-  storyName?: string;
-}
-
-const props = defineProps<Props>();
-const drafts = useDraftsStore();
-
-const story = computed(() => props.storyName ?? drafts.story.name);
+defineProps<{
+  primary: Record<string, string>[];
+  secondary: Record<string, string>[];
+}>();
 </script>
