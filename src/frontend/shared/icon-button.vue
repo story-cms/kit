d7c4f08b4bd0d70b6eb64b9986e033185cb1e177
Timothy Koech <timosville@gmail.com>
2025-07-28 14:20:52 +0300

    feat: add IconButton component for icon-based interactions

---
+++ src/frontend/shared/icon-button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    type="button"
+    class="rounded-full bg-blue-100 p-2 shadow-md hover:bg-blue-200"
+    @click="emit('tap')"
+  >
+    <Icon :name="icon" class="text-blue-800" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@story-cms/kit/ui';
+
+defineProps<{
+  icon: string;
+}>();
+
+const emit = defineEmits<{
+  (e: 'tap'): void;
+}>();
+</script>
