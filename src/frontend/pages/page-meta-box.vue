69cacbded72ec5aa59b696d265480ac665f0ecd7
Timothy Koech <timosville@gmail.com>
2025-07-30 14:49:16 +0300

    refactor: replace page-meta-box content with MetaBox component and update story examples for primary and secondary data binding

--- src/frontend/pages/page-meta-box.vue
+++ src/frontend/pages/page-meta-box.vue
@@ -1,28 +1,11 @@
 <template>
-  <div class="rounded-lg border border-gray-100 font-['Inter']">
-    <section
-      class="grid grid-cols-1 p-4 text-xs font-normal leading-4 bg-gray-200 gap-y-2"
-    >
-      <div class="grid grid-cols-2">
-        <p>Created</p>
-        <span class="text-right place-self-end">{{ formatDate(props.createdAt) }}</span>
-      </div>
-      <div class="grid grid-cols-2">
-        <p>Auto-Saved</p>
-        <span class="text-right place-self-end">{{ formatDate(props.updatedAt) }}</span>
-      </div>
-      <div class="grid grid-cols-2">
-        <p>Last Published</p>
-        <span class="text-right place-self-end">{{ publishedAtLabel }}</span>
-      </div>
-    </section>
-  </div>
+  <MetaBox :primary="primary" />
 </template>
 <script setup lang="ts">
 import { computed } from 'vue';
 import type { PageMeta } from '../../types';
 import { formatDate } from '../shared/helpers';
-
+import MetaBox from '../shared/meta-box.vue';
 const props = defineProps<
   Pick<PageMeta, 'createdAt' | 'updatedAt'> & {
     savedAt: string;
@@ -34,5 +17,13 @@ const props = defineProps<
 const publishedAtLabel = computed(() => {
   if (props.publishedAt === 'unpublished') return 'UNPUBLISHED';
   return formatDate(props.publishedAt);
+});
+
+const primary = computed(() => {
+  return [
+    { label: 'Created', value: formatDate(props.createdAt) },
+    { label: 'Auto-Saved', value: formatDate(props.updatedAt) },
+    { label: 'Last Published', value: publishedAtLabel.value },
+  ];
 });
 </script>
