f5a4b5a4abfab09b062e40b246451eca10dc43fd
Timothy Koech <timosville@gmail.com>
2025-08-04 15:42:07 +0300

    chore: sort imports

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,46 +1,47 @@
-import AppLayout from './shared/app-layout.vue';
-import Icon from './shared/icon.vue';
-import DropDown from './shared/drop-down.vue';
-import StringField from './fields/string-field.vue';
-import ContextMenu from './shared/context-menu.vue';
-import UsersIndex from './team/users-index.vue';
-import StoryIndex from './stories/story-index.vue';
-import DashboardIndex from './dashboard/dashboard-index.vue';
-import Login from './auth/login-index.vue';
-import CreateAccount from './auth/create-account.vue';
-import ForgotPassword from './auth/forgot-password.vue';
-import ResetPassword from './auth/reset-password.vue';
-import IndexCard from './stories/components/index-card.vue';
-import StatusTag from './stories/components/status-tag.vue';
+import ActionButton from './shared/action-button.vue';
+import AddItemButton from './shared/add-item-button.vue';
 import AnimationField from './fields/animation-field.vue';
+import AppLayout from './shared/app-layout.vue';
+import AudiencesIndex from './audiences/audiences-index.vue';
 import AudioField from './fields/audio-field.vue';
-import ImageField from './fields/image-field.vue';
 import BooleanField from './fields/boolean-field.vue';
+import ChapterPreview from './stories/chapter-preview.vue';
+import ContextMenu from './shared/context-menu.vue';
+import CreateAccount from './auth/create-account.vue';
+import DashboardIndex from './dashboard/dashboard-index.vue';
+import DateField from './fields/date-field.vue';
+import DraftIndex from './stories/draft-index.vue';
+import DropDown from './shared/drop-down.vue';
+import ForgotPassword from './auth/forgot-password.vue';
+import Icon from './shared/icon.vue';
+import IconButton from './shared/icon-button.vue';
+import ImageField from './fields/image-field.vue';
+import IndexCard from './stories/components/index-card.vue';
+import IndexFilter from './shared/index-filter.vue';
+import LabelButton from './shared/label-button.vue';
 import ListField from './fields/list-field.vue';
+import ListSwitcher from './shared/list-switcher.vue';
+import Login from './auth/login-index.vue';
 import MarkdownField from './fields/markdown-field.vue';
+import MetaBox from './shared/meta-box.vue';
 import NullField from './fields/null-field.vue';
 import NumberField from './fields/number-field.vue';
 import ObjectField from './fields/object-field.vue';
+import PageIndexItem from './pages/page-index-item.vue';
+import PagesEdit from './pages/pages-edit.vue';
+import PagesIndex from './pages/pages-index.vue';
 import PanelField from './fields/panel-field.vue';
+import PublicLayout from './shared/public-layout.vue';
+import ResetPassword from './auth/reset-password.vue';
 import ScriptureField from './fields/scripture-field.vue';
 import SelectField from './fields/select-field.vue';
-import IndexFilter from './shared/index-filter.vue';
-import AddItemButton from './shared/add-item-button.vue';
-import PublicLayout from './shared/public-layout.vue';
-import PageIndexItem from './pages/page-index-item.vue';
-import DraftIndex from './stories/draft-index.vue';
+import StatusTag from './stories/components/status-tag.vue';
+import StoryIndex from './stories/story-index.vue';
+import StringField from './fields/string-field.vue';
 import TranslationIndex from './stories/translation-index.vue';
-import PagesIndex from './pages/pages-index.vue';
-import PagesEdit from './pages/pages-edit.vue';
-import ChapterPreview from './stories/chapter-preview.vue';
 import UiPage from './ui/ui-page.vue';
+import UsersIndex from './team/users-index.vue';
 import VideoField from './fields/video-field.vue';
-import DateField from './fields/date-field.vue';
-import ActionButton from './shared/action-button.vue';
-import IconButton from './shared/icon-button.vue';
-import LabelButton from './shared/label-button.vue';
-import ListSwitcher from './shared/list-switcher.vue';
-import MetaBox from './shared/meta-box.vue';
 
 export { createInertiaApp, usePage, router } from '@inertiajs/vue3';
 
@@ -49,47 +50,48 @@ export * from './store/index';
 export { commonProps } from './shared/helpers';
 
 export {
-  AppLayout,
-  UsersIndex,
-  Icon,
-  DropDown,
-  ContextMenu,
-  StringField,
-  Login,
-  CreateAccount,
-  ForgotPassword,
-  ResetPassword,
-  StoryIndex,
-  DashboardIndex,
-  IndexCard,
-  StatusTag,
+  ActionButton,
+  AddItemButton,
   AnimationField,
+  AppLayout,
+  AudiencesIndex,
   AudioField,
-  ImageField,
   BooleanField,
+  ChapterPreview,
+  ContextMenu,
+  CreateAccount,
+  DashboardIndex,
+  DateField,
+  DraftIndex,
+  DropDown,
+  ForgotPassword,
+  Icon,
+  IconButton,
+  ImageField,
+  IndexCard,
+  IndexFilter,
+  LabelButton,
   ListField,
+  ListSwitcher,
+  Login,
   MarkdownField,
+  MetaBox,
   NullField,
   NumberField,
   ObjectField,
+  PageIndexItem,
+  PagesEdit,
+  PagesIndex,
   PanelField,
+  PublicLayout,
+  ResetPassword,
   ScriptureField,
   SelectField,
-  VideoField,
-  IndexFilter,
-  AddItemButton,
-  PublicLayout,
-  PageIndexItem,
-  DraftIndex,
+  StatusTag,
+  StoryIndex,
+  StringField,
   TranslationIndex,
-  PagesIndex,
-  PagesEdit,
-  ChapterPreview,
   UiPage,
-  DateField,
-  ActionButton,
-  IconButton,
-  LabelButton,
-  ListSwitcher,
-  MetaBox,
+  UsersIndex,
+  VideoField,
 };
