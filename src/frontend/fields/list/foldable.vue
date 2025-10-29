a37cfacd8092756950fa68e4ade1c06f5996d602
Timothy Koech <timosville@gmail.com>
2025-10-29 13:02:12 +0300

    feat: implement foldable list component with item management and dynamic toggling

---
+++ src/frontend/fields/list/foldable.vue
@@ -0,0 +1,177 @@
+<template>
+  <ul
+    v-for="(_listItem, index) in listItems"
+    :key="index"
+    class="row-[span_100] mb-8 grid grid-rows-subgrid"
+  >
+    <li class="row-[span_100] grid grid-rows-subgrid">
+      <div class="relative flex items-center justify-between">
+        <span class="absolute left-0 right-0 top-[19px] border-t border-gray-300"></span>
+        <button
+          type="button"
+          class="z-[1] inline-flex items-center rounded-full border border-gray-300 bg-white px-4 py-1.5 text-sm font-medium leading-5 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          :disabled="isReadOnly"
+          :class="{ 'opacity-0': isReadOnly }"
+          @click.prevent="toggle(index)"
+        >
+          <icon
+            v-if="isExpanded(index)"
+            name="chevron-down"
+            class="icon mr-1"
+            aria-hidden="true"
+          />
+          <icon v-else name="chevron-right" class="icon mr-1" aria-hidden="true" />
+          <span>
+            {{ String(sectionTitle(index)) }}
+          </span>
+        </button>
+        <div v-if="itemHasError(index)" class="text-accent-one">
+          <div class="rounded-full border bg-white p-2">
+            <Icon name="exclamation" class="h-10 w-10 text-red-500" />
+          </div>
+        </div>
+        <button
+          v-if="canMutate"
+          type="button"
+          class="z-[1] flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border bg-white text-gray-500"
+          @click="emit('removeSet', index)"
+        >
+          <span v-if="!isReadOnly" class="flex h-10 w-10 items-center justify-center">
+            <Icon name="trash" class="h-auto w-auto" />
+          </span>
+        </button>
+      </div>
+      <ul v-if="isExpanded(index)" class="row-[span_100] grid grid-rows-subgrid">
+        <li
+          v-for="(item, i) in fields"
+          :key="item.name + `${i.toString()}`"
+          class="grid grid-rows-subgrid"
+          :style="{ gridRow: `span ${listItems.length}` }"
+        >
+          <component
+            :is="widgets.picker(item.widget)"
+            :class="{
+              'rounded bg-white': item.widget != 'list',
+              'rounded bg-white p-8': isIsland(item.widget),
+            }"
+            :field="item"
+            :is-read-only="props.isReadOnly"
+            :root-path="`${fieldPath}.${index.toString()}`"
+            :is-nested="true"
+          />
+        </li>
+      </ul>
+    </li>
+  </ul>
+  <div v-if="canMutate" class="flex items-center gap-4">
+    <AddItemButton :label="field.label" @add="emit('addSet')" />
+    <div v-if="showEmptyListWarning()">
+      <div class="flex flex-row items-center rounded-full border bg-white p-2 text-error">
+        <Icon name="exclamation" class="pr-2" />
+        <p class="text-sm">At least one item is required</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { PropType } from 'vue';
+import type { FieldSpec } from '../../../types';
+import Icon from '../../shared/icon.vue';
+import { useModelStore, useWidgetsStore, useSharedStore } from '../../store';
+import AddItemButton from '../../shared/add-item-button.vue';
+
+const props = defineProps({
+  field: {
+    type: Object as PropType<FieldSpec>,
+    required: true,
+  },
+  fieldPath: {
+    type: String,
+    required: true,
+  },
+  listItems: {
+    type: Array,
+    required: true,
+  },
+  isReadOnly: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+const emit = defineEmits(['addSet', 'removeSet']);
+const field = computed(() => props.field as FieldSpec);
+const fields = field.value.fields as FieldSpec[];
+const model = useModelStore();
+const widgets = useWidgetsStore();
+const shared = useSharedStore();
+
+const canMutate = computed(() => {
+  if (props.isReadOnly) return false;
+
+  return !shared.isTranslation;
+});
+
+const isIsland = (type: string): boolean => {
+  const singleWidgets = ['string', 'number', 'markdown', 'image', 'boolean', 'select'];
+  return singleWidgets.includes(type);
+};
+
+widgets.setListToggles(props.fieldPath, [false, false, false, false, false]);
+const toggleState = computed(() => widgets.getListToggles(props.fieldPath));
+
+const isExpanded = (index: number): boolean => {
+  return toggleState.value[index];
+};
+
+const toggle = (index: number) => {
+  const fresh = toggleState.value;
+  fresh[index] = !fresh[index];
+
+  widgets.setListToggles(props.fieldPath, fresh);
+};
+
+const sectionTitle = (index: number): string => {
+  const peek = title(index);
+  return `${field.value.label} : ${peek}`;
+};
+
+const title = (index: number): string => {
+  let path = `${props.fieldPath}.${index.toString()}`;
+  if (field.value.index) {
+    path = `${path}.${field.value.index}`;
+  } else {
+    path = `${path}.${fields[0].name}`;
+  }
+  const itemTitle = model.getField(path, 'New Section') as string;
+
+  return itemTitle.length > 20 ? `${itemTitle.substring(0, 20)}...` : itemTitle;
+};
+
+const itemHasError = (index: number): boolean => {
+  if (props.isReadOnly) return false;
+  for (const key in shared.errors) {
+    const needle = `bundle.${props.fieldPath}.${index}`;
+    if (key.startsWith(needle)) return true;
+  }
+  return false;
+};
+
+const showEmptyListWarning = (): boolean => {
+  if (props.isReadOnly) return false;
+  if (props.listItems.length > 0) return false;
+
+  for (const key in shared.errors) {
+    const needle = `bundle.${props.fieldPath}`;
+    if (key.startsWith(needle)) return true;
+  }
+  return false;
+};
+
+const borderWidth = computed(() => {
+  if (shared.showSourceColumn) return `${shared.headerWidth - 30}px`;
+  return '100%';
+});
+</script>
