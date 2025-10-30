e105e3fe608f505abef822b66f8e646e467c9a44
Timothy Koech <timosville@gmail.com>
2025-10-30 09:45:05 +0300

    refactor: enhance layout structure and item management in flat list component

--- src/frontend/fields/list/flat-list.vue
+++ src/frontend/fields/list/flat-list.vue
@@ -1,34 +1,38 @@
 <template>
-  <ul
-    v-for="(_listItem, index) in listItems"
-    :key="index"
-    role="listitem"
-    class="my-2 grid gap-y-8 bg-gray-100 p-8"
-  >
-    <div
-      v-if="canMutate"
-      class="absolute right-0 mr-3 cursor-pointer text-gray-500"
-      @click="emit('removeSet', index)"
+  <div class="ml-8">
+    <ul
+      v-for="(_listItem, index) in listItems"
+      :key="index"
+      role="listitem"
+      class="my-2 grid gap-y-8 bg-gray-100 p-8"
     >
-      <Icon name="trash" class="h-10 w-10" />
-    </div>
+      <div
+        v-if="canMutate"
+        class="absolute right-0 mr-3 cursor-pointer text-gray-500"
+        @click="emit('removeSet', index)"
+      >
+        <Icon name="trash" class="h-10 w-10" />
+      </div>
 
-    <li v-for="(item, i) in fields" :key="item.name + `${i.toString()}`">
-      <component
-        :is="store.picker(item.widget)"
-        :field="item"
-        :is-read-only="props.isReadOnly"
-        :root-path="`${fieldPath}.${index.toString()}`"
-        :is-nested="true"
-      />
-    </li>
-  </ul>
-  <div v-if="canMutate" class="ml-8 mt-8 flex flex-row items-center gap-4">
-    <AddItemButton :label="field.label" @add="emit('addSet')" />
-    <div v-if="showEmptyListWarning()">
-      <div class="flex flex-row items-center rounded-full border bg-white p-2 text-error">
-        <Icon name="exclamation" class="pr-2" />
-        <p class="text-sm">At least one item is required</p>
+      <li v-for="(item, i) in fields" :key="item.name + `${i.toString()}`">
+        <component
+          :is="store.picker(item.widget)"
+          :field="item"
+          :is-read-only="props.isReadOnly"
+          :root-path="`${fieldPath}.${index.toString()}`"
+          :is-nested="true"
+        />
+      </li>
+    </ul>
+    <div v-if="canMutate" class="mt-8 flex flex-row items-center gap-4">
+      <AddItemButton :label="field.label" @add="emit('addSet')" />
+      <div v-if="showEmptyListWarning()">
+        <div
+          class="flex flex-row items-center rounded-full border bg-white p-2 text-error"
+        >
+          <Icon name="exclamation" class="pr-2" />
+          <p class="text-sm">At least one item is required</p>
+        </div>
       </div>
     </div>
   </div>
