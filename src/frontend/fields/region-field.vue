cb65db833885bf6cadf312da778243cb559b276f
Timothy Koech <timosville@gmail.com>
2025-12-16 06:38:22 +0300

    refactor: adjust spacing and alignment in RegionField component

--- src/frontend/fields/region-field.vue
+++ src/frontend/fields/region-field.vue
@@ -15,7 +15,7 @@
 
     <div class="relative">
       <div
-        class="mt-[2px] grid gap-x-2 rounded-md border border-gray-300 bg-white pb-1 pt-1"
+        class="mt-[2px] grid gap-x-1 rounded-md border border-gray-300 bg-white pb-1 pt-1"
         :class="
           tags.length > 0 && !props.isReadOnly
             ? 'grid-cols-[auto_minmax(32%,_1fr)]'
@@ -23,8 +23,8 @@
         "
       >
         <div
-          class="flex flex-wrap items-center gap-2 text-base text-gray-500 sm:text-sm/6"
-          :class="tags.length > 0 ? 'pl-3' : 'px-0'"
+          class="flex flex-wrap gap-2 items-center text-base text-gray-500 sm:text-sm/6"
+          :class="tags.length > 0 ? 'pl-1' : 'px-0'"
           @click="inputRef?.focus()"
         >
           <span
@@ -60,7 +60,8 @@
           v-model="newTag"
           type="text"
           :name="field.label"
-          class="mr-1 block rounded-r-md border-0 bg-white py-1 pl-0 text-sm font-normal leading-5 text-gray-900 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white"
+          class="block py-1 pl-0 text-sm font-normal leading-5 text-gray-900 bg-white rounded-r-md border-0 focus:mr-1 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white"
+          :class="tags.length > 0 ? 'ml-0' : 'ml-1'"
           @keydown.enter.stop="handleEnter"
           @keydown.arrow-down.prevent="navigateDown"
           @keydown.arrow-up.prevent="navigateUp"
@@ -72,7 +73,7 @@
       </div>
       <div
         v-if="showDropdown && filteredRegions.length > 0 && !props.isReadOnly"
-        class="absolute z-10 mt-1 w-full rounded-md border border-gray-300 bg-white py-1 shadow-lg"
+        class="absolute z-10 py-1 mt-1 w-full bg-white rounded-md border border-gray-300 shadow-lg"
       >
         <button
           v-for="(region, index) in filteredRegions"
