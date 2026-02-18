15cc9bcf13a0fe3b4f72ebaf39a2414c1a4287b6
Jannie Theunissen <jannie@scoutredeem.co>
2026-02-18 17:23:41 +0200

    feat: dynamic config

--- src/backend/providers/cms_provider.ts
+++ src/backend/providers/cms_provider.ts
@@ -1,17 +1,34 @@
 import { CmsService } from '../services/cms_service.js';
 import type { ApplicationService } from '@adonisjs/core/types';
+import Config from '../models/config.js';
+import { CmsConfig } from '../../types.js';
 
 export default class CmsProvider {
   constructor(protected app: ApplicationService) {}
 
   register() {
     this.app.container.singleton(CmsService, async (resolver) => {
-      // currently reading from config file
-      // NEXT: read from database
-      const configService = await resolver.make('config');
-      const cmsConfig = configService.get<any>('cms');
+      // check to see if we have a config in the database
+      const persisted = await Config.query()
+        .where('key', 'cms')
+        .orderBy('version', 'desc')
+        .first();
 
-      return new CmsService(cmsConfig);
+      let cmsConfig: CmsConfig;
+      if (persisted) {
+        cmsConfig = persisted.data as CmsConfig;
+      } else {
+        // save the default config to the database
+        const configService = await resolver.make('config');
+        cmsConfig = configService.get<any>('cms') as CmsConfig;
+        await Config.create({
+          key: 'cms',
+          version: 1,
+          data: cmsConfig as Record<string, any>,
+        });
+      }
+
+      return new CmsService(cmsConfig!);
     });
   }
 }
