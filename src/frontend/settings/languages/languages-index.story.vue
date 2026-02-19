8472ff9ea9e3d03f7dd1a63b0f77e99da8b8e6f6
Timothy Koech <timosville@gmail.com>
2026-02-19 14:52:15 +0300

    feat: add settings section and languages index component

---
+++ src/frontend/settings/languages/languages-index.story.vue
@@ -0,0 +1,12 @@
+<template>
+  <Story title="Languages Page" group="settings">
+    <Variant title="Index" :setup-app="miniSidebar">
+      <LanguagesIndex />
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import LanguagesIndex from './languages-index.vue';
+import { miniSidebar } from '../../test/mocks';
+</script>
