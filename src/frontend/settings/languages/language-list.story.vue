7730b9c9023561bb9aedc3a6ae09f387fd3bdea8
Timothy Koech <timosville@gmail.com>
2026-02-25 09:24:12 +0300

    feat: add language list component and corresponding story for language display

---
+++ src/frontend/settings/languages/language-list.story.vue
@@ -0,0 +1,12 @@
+<template>
+    <Story title="Language List" group="settings">
+        <Variant title="List" :setup-app="miniSidebar">
+            <LanguageList />
+        </Variant>
+    </Story>
+</template>
+
+<script setup lang="ts">
+import { miniSidebar } from '../../test/mocks';
+import LanguageList from './language-list.vue';
+</script>
---
+++ src/frontend/settings/languages/language-list.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <h2>Language List</h2>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
