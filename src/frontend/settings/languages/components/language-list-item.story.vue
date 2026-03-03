aa1322099d68a69456932cacb61e1f73b0997f44
Timothy Koech <timosville@gmail.com>
2026-03-03 08:15:32 +0300

    feat: add interactive variant to language-list-item story for enhanced selection demonstration

--- src/frontend/settings/languages/components/language-list-item.story.vue
+++ src/frontend/settings/languages/components/language-list-item.story.vue
@@ -39,9 +39,26 @@
         :is-added="true"
         />
     </Variant>
+    <Variant title="Interactive">
+      <LanguageListItem
+        :language="{
+          language: 'English',
+          locale: 'en',
+          languageDirection: 'ltr',
+          bibleVersionId: 'de4e12af7f28f599-01',
+          bibleLabel: '(KJV) King James Version',
+        }"
+        :is-selected="isSelected"
+        :is-added="false"
+        @update="isSelected = $event"
+      />
+    </Variant>
   </Story>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import LanguageListItem from './language-list-item.vue';
+
+const isSelected = ref(false);
 </script>
