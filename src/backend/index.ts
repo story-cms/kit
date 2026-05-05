d57b170729881e5dd718b31810bb08bbd8761ae3
Jannie Theunissen <jannie@scoutredeem.co>
2026-05-05 17:23:34 +0200

    fix: export

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -41,7 +41,7 @@ export * from './validators/auth.js';
 export * from './validators/campaign.js';
 export * from './validators/bundle.js';
 export * from './validators/chapter.js';
-export * from './validators/drop.js';
+export { default as DropValidator } from './validators/drop.js';
 export { default as PageValidator } from './validators/page.js';
 export * from './validators/user.js';
 export { default as audioRule } from './validators/audio_rule.js';
