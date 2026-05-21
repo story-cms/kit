71acb27b7a53cd988cb9cd4258748429c6060c89
Jannie Theunissen <jannie@scoutredeem.co>
2026-05-21 11:38:19 +0200

    increment: variant for StoryTransformer

--- src/backend/transformers/story_transformer.ts
+++ src/backend/transformers/story_transformer.ts
@@ -1,8 +1,68 @@
 import { BaseTransformer } from '@adonisjs/core/transformers';
 import Story from '../models/story';
+import StoryLocalisation from '../models/story_localisation';
 
 export class StoryTransformer extends BaseTransformer<Story> {
   toObject() {
     return this.omit(this.resource, ['order', 'localisations']);
   }
+
+  async forStoryEdit() {
+    const target =
+      this.resource.localisations.find(
+        (localisation) => localisation.locale === this.resource.$sideloaded.targetlocale,
+      ) ?? emptyLocalisation;
+    const source =
+      this.resource.localisations.find(
+        (localisation) => localisation.locale === this.resource.$sideloaded.sourcelocale,
+      ) ?? emptyLocalisation;
+    return {
+      model: {
+        ...this.pick(this.resource, [
+          'id',
+          'tags',
+          'chapterLimit',
+          'storyType',
+          'chapterType',
+          'sectionType',
+          'visibility',
+          'slug',
+          'template',
+          'isPublished',
+          'createdAt',
+          'updatedAt',
+        ]),
+        ...this.pick(target, [
+          'title',
+          'coverImage',
+          'description',
+          'sections',
+          'resources',
+        ]),
+      },
+      source: {
+        ...this.pick(source, [
+          'title',
+          'coverImage',
+          'description',
+          'sections',
+          'resources',
+        ]),
+      },
+    };
+  }
 }
+
+const emptyLocalisation: Partial<StoryLocalisation> = {
+  title: '',
+  coverImage: '',
+  description: '',
+  sections: [],
+  resources: [],
+};
+
+//   localisations: this.resource.localisations.map((localisation) => ({
+//   ...this.pick(localisation, ['locale', 'title', 'coverImage', 'description']),
+//   sections: localisation.sections,
+//   resources: localisation.resources,
+// })),
