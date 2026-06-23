5c4ca383fcbbe982ccddb380e320d66a5004ad6a
Timothy Koech <timosville@gmail.com>
2026-06-23 08:56:02 +0300

    refactor: simplify ResourceEmptyState component and remove unused browse functionality

--- src/frontend/stories/components/story-edit-resources.vue
+++ src/frontend/stories/components/story-edit-resources.vue
@@ -2,17 +2,13 @@
   <div class="grid min-w-0 grid-cols-[minmax(0,1fr)] overflow-x-clip">
     <div class="min-w-0 space-y-6">
       <ResourcePicker
-        ref="resourcePicker"
         :available-resources="availableResources"
         :attached-resources="attachedResources"
         @attach="attachResource"
         @create="emit('create')"
       />
 
-      <ResourceEmptyState
-        v-if="attachedResources.length === 0"
-        @browse="resourcePicker?.openSearch()"
-      />
+      <ResourceEmptyState v-if="attachedResources.length === 0" />
 
       <ResourceAttachedList v-else v-model:resources="attachedResources" />
     </div>
@@ -20,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import ResourceAttachedList from './resource-attached-list.vue';
 import ResourceEmptyState from './resource-empty-state.vue';
 import ResourcePicker from './resource-picker.vue';
@@ -35,8 +31,6 @@ const emit = defineEmits<{
   'update:resources': [resources: ResourceItem[]];
   create: [];
 }>();
-
-const resourcePicker = ref<InstanceType<typeof ResourcePicker> | null>(null);
 
 const attachedResources = computed({
   get: () => props.resources,
