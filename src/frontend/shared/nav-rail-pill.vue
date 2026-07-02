21139b225dedc7ebf3e79cd3af8c208cfe90d283
Timothy Koech <timosville@gmail.com>
2026-07-02 11:23:10 +0300

    refactor: simplify sidebar component by replacing with NavRailPill and NavRailPanel

---
+++ src/frontend/shared/nav-rail-pill.vue
@@ -0,0 +1,13 @@
+<template>
+  <aside
+    class="fixed inset-y-0 z-20 left-3 top-3 mx-auto max-h-min rounded-full bg-white shadow-md transition-all duration-75"
+  >
+    <nav>
+      <NavRailToolbar variant="pill" />
+    </nav>
+  </aside>
+</template>
+
+<script setup lang="ts">
+import NavRailToolbar from './nav-rail-toolbar.vue';
+</script>
